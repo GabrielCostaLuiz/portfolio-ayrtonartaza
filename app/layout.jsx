438c44516246ee929f65e0b808e73e61a7c3fe47
gabrielcostaluiz <gabrielbragacostaluiz@gmail.com.br>
2025-01-05 03:21:18 -0300

    Arrumado fonts

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,18 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google"
+import { Inter } from 'next/font/google'
 import "./globals.css"
 import { name } from "../utils/constants"
 import Footer from "../components/footer"
 import NavBar from "../components/navBar"
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-})
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-})
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: {
@@ -27,7 +19,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased container m-auto py-10 max-sm:px-5 `}
+        className={`${inter.variable} antialiased container m-auto py-10 max-sm:px-5 `}
       >
         <header>
           <NavBar />
