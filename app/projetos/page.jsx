6bf91dbd5f157bcad9a0ebaacec7b2220d137258
gabrielcostaluiz <gabrielbragacostaluiz@gmail.com.br>
2025-01-05 02:44:20 -0300

    Pagina projetos arrumada

--- app/projetos/page.jsx
+++ app/projetos/page.jsx
@@ -1,28 +1,28 @@
 import CardProjects from "../../components/cardProjects"
 import { getRepos } from "../actions/getRepositories"
 
-export default function Projetos() {
-//   const repositorios = await getRepos()
+export default async function Projetos() {
+  const repositorios = await getRepos()
 
-//   if (repositorios.error) {
-//     return (
-//       <section className="mt-32">
-//         <h2>Projetos</h2>
-//         <div className="text-center text-red-600 font-bold">
-//           {repositorios.error}
-//         </div>
-//       </section>
-//     )
-//   }
+  if (repositorios.error) {
+    return (
+      <section className="mt-32">
+        <h2>Projetos</h2>
+        <div className="text-center text-red-600 font-bold">
+          {repositorios.error} 
+        </div>
+      </section>
+    )
+  }
 
   return (
     <section className="mt-32">
-      <h2>Projetos</h2>
+      <h2>Projetos</h2> 
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-      
-         <CardProjects />
-   
+        {repositorios.map((repositorio) => {
+          return <CardProjects key={repositorio.id} repositorio={repositorio} />
+        })}
       </div>
     </section>
   )
